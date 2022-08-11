815ac9fd9cb1985c2394bb91d835eb9637192cb7
yadavshi <57186120+yadavshi@users.noreply.github.com>
2022-08-10 22:33:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ Advisor: Dr. Arun Ross
 
 Reference:
 StarGAN v2
-Copyright (c) 2020-present NAVER Corp.
-
-This work is licensed under the Creative Commons Attribution-NonCommercial
-4.0 International License. To view a copy of this license, visit
-http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
-Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+https://github.com/clovaai/stargan-v2
 """
 
 import os
